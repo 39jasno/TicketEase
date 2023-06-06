9222892761b969a181a301a4c73387b7fcf8c30b
jasno10 <jasonmiran09@gmail.com>
2023-06-06 16:46:03 +0800

    ertg

--- firstfood.cs
+++ firstfood.cs
@@ -4,10 +4,6 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace TicketEase
 {
