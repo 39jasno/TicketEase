3b0a6a353426f6fd0fd5c10f2e87751331d8631e
jasno10 <jasonmiran09@gmail.com>
2023-06-07 19:20:12 +0800

    Login and Signup Fix

--- MainActivity.cs
+++ MainActivity.cs
@@ -3,27 +3,21 @@ using System.IO;
 using System.Net;
 using Android.App;
 using Android.Content;
-using Android.Icu.Text;
 using Android.OS;
 using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using AndroidX.AppCompat.App;
-using Google.Android.Material.BottomNavigation;
-using Java.Security;
 
 namespace TicketEase
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        TextView textMessage;
-        TextView text1;
         EditText edit1, edit2;
         Button btn1, btn2;
         HttpWebResponse response;
         HttpWebRequest request;
-        string res = "", str = "", user_email = "", user_pass = "";
+        string res = "", user_username="", user_email = "", user_pass = "";
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -31,22 +25,21 @@ namespace TicketEase
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.LoginUI);
 
-            text1 = FindViewById<TextView>(Resource.Id.textView1);
-            edit1 = FindViewById<EditText>(Resource.Id.edituser);
-            edit2 = FindViewById<EditText>(Resource.Id.editpass);
-            btn1 = FindViewById<Button>(Resource.Id.btnlogin);
-            btn2 = FindViewById<Button>(Resource.Id.btnsignup);
+            edit1 = FindViewById<EditText>(Resource.Id.editUser);
+            edit2 = FindViewById<EditText>(Resource.Id.editPass);
+            btn1 = FindViewById<Button>(Resource.Id.btnLogin);
+            btn2 = FindViewById<Button>(Resource.Id.btnSignup);
 
             btn1.Click += this.Login;
             btn2.Click += this.Signup;
         }
         public void Login(object sender, EventArgs e)
         {
-            user_email = edit1.Text;
+            user_username = edit1.Text;
             user_pass = edit2.Text;
             //192.168.1.32
             //192.168.1.50
-            request = (HttpWebRequest)WebRequest.Create("http://192.168.1.32:8080/ticketease/rest/admin_login.php?user_email=" + user_email + "&user_pass=" + user_pass);
+            request = (HttpWebRequest)WebRequest.Create("http://192.168.1.50:8080/ticketease/rest/login.php?user_username=" + user_username +  "&user_pass=" + user_pass);
             response = (HttpWebResponse)request.GetResponse();
             StreamReader reader = new StreamReader(response.GetResponseStream());
             res = reader.ReadToEnd();
--- Signup.cs
+++ Signup.cs
@@ -16,12 +16,12 @@ namespace TicketEase
     [Activity(Label = "Signup")]
     public class Signup : Activity
     {
-        EditText newfname, newlname, newbdate, newnumber, newemail, newpass;
+        EditText newfname, newlname, newmonth, newday, newyear, newnumber, newemail, newuser, newpass;
         RadioGroup gender;
         Button signup;
         HttpWebResponse response;
         HttpWebRequest request;
-        String user_fname = "", user_lname = "", user_bdate = "", user_number = "", user_email = "", user_pass = "", selectedGender = "", res = "";
+        String user_fname = "", user_lname = "", user_gender ="", user_month = "", user_day = "", user_year = "", user_number = "", user_username="", user_email = "", user_pass = "", selectedGender = "", res = "";
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -32,12 +32,16 @@ namespace TicketEase
             //instantiate widgets
             newfname = FindViewById<EditText>(Resource.Id.editText1);
             newlname = FindViewById<EditText>(Resource.Id.editText2);
-            newbdate = FindViewById<EditText>(Resource.Id.editText3);
             gender = FindViewById<RadioGroup>(Resource.Id.radioGroup1);
             gender.CheckedChange += myRadioGroup_CheckedChange;
-            newnumber = FindViewById<EditText>(Resource.Id.editText4);
-            newemail = FindViewById<EditText>(Resource.Id.editText5);
-            newpass = FindViewById<EditText>(Resource.Id.editText6);
+            newmonth = FindViewById<EditText>(Resource.Id.editText3);
+            newday = FindViewById<EditText>(Resource.Id.editText4);
+            newyear = FindViewById<EditText>(Resource.Id.editText5);
+            newnumber = FindViewById<EditText>(Resource.Id.editText6);
+            newemail = FindViewById<EditText>(Resource.Id.editText7);
+            newuser = FindViewById<EditText>(Resource.Id.editText8);
+            newpass = FindViewById<EditText>(Resource.Id.editText9);
+
             signup = FindViewById<Button>(Resource.Id.button1);
 
 
@@ -50,7 +54,19 @@ namespace TicketEase
         {
             int checkedItemId = gender.CheckedRadioButtonId;
             RadioButton checkedRadioButton = FindViewById<RadioButton>(checkedItemId);
-            selectedGender = checkedItemId.ToString();
+            if (checkedRadioButton != null)
+            {
+                // Assign the appropriate value to selectedGender based on the selected RadioButton
+                if (checkedRadioButton.Text == "Male")
+                {
+                    selectedGender = "Male";
+                }
+                else if (checkedRadioButton.Text == "Female")
+                {
+                    selectedGender = "Female";
+                }
+            }
+            //selectedGender = checkedItemId.ToString();
             gender.Check(checkedItemId);
 
 
@@ -59,12 +75,16 @@ namespace TicketEase
         {
             user_fname = newfname.Text;
             user_lname = newlname.Text;
-            user_bdate = newbdate.Text;
+            user_gender = selectedGender;
+            user_month = newmonth.Text;
+            user_day = newday.Text;
+            user_year = newyear.Text;
             user_number = newnumber.Text;
             user_email = newemail.Text;
+            user_username = newuser.Text;
             user_pass = newpass.Text;
 
-            request = (HttpWebRequest)WebRequest.Create("http://192.168.1.50:8080/ticketease/rest/add_record.php?user_fname=" + user_fname + "&user_lname=" + user_lname + "&user_gender=" + gender + "&user_bdate=" + user_bdate + "&user_number=" + user_number + "&user_email=" + user_email + "&user_pass=" + user_pass);
+            request = (HttpWebRequest)WebRequest.Create("http://192.168.1.50:8080/ticketease/rest/signup.php?user_fname=" + user_fname + "&user_lname=" + user_lname + "&user_gender=" + selectedGender + "&user_month=" + user_month + "&user_day=" + user_day + "&user_year=" + user_year + "&user_number=" + user_number + "&user_email=" + user_email + "&user_username=" +user_username + "&user_pass=" + user_pass);
             response = (HttpWebResponse)request.GetResponse();
             StreamReader reader = new StreamReader(response.GetResponseStream());
             res = reader.ReadToEnd();
